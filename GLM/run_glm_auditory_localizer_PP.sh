dfee523d4e1e40784effb91d83dcd3797145d92e
Aryan Agarwal <aryan.agarwal@yale.edu>
2026-09-16 22:31:11 -0400

    glm condition level and glm single

--- GLM/run_glm_auditory_localizer_PP.sh
+++ GLM/run_glm_auditory_localizer_PP.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH --output logs/%j-GLM-visual-localizer_%A_%a.out
-#SBATCH --job-name GLM_VISUAL_LOCALIZER-PP
+#SBATCH --output logs/%j-GLM-auditory-localizer_%A_%a.out
+#SBATCH --job-name GLM_AUDITORY_LOCALIZER-PP
 #SBATCH --array=1-1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=30G
@@ -11,10 +11,10 @@
 module load miniconda
 conda activate "/gpfs/milgram/project/turk-browne/$1/conda_envs/myenv"
 
-SUBJECTS=("pp05")
+SUBJECTS=("pp06")
 
 SUBJECT_ID=${SUBJECTS[$SLURM_ARRAY_TASK_ID - 1]}
 
-echo "Running visual localizer GLM for subject $SUBJECT_ID"
+echo "Running auditory localizer GLM for subject $SUBJECT_ID"
 
-python "/gpfs/milgram/project/turk-browne/$1/auditory-objects-scenes-project/GLM/glm_visual_localizer.py" -s "$SUBJECT_ID"
+python "/gpfs/milgram/project/turk-browne/$1/auditory-objects-scenes-project/GLM/glm_auditory_localizer.py" -s "$SUBJECT_ID"
